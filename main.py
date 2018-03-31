250d5faad409a3dd806b1c4d552a64e83ab213a8
Derek.S <derekseli@outlook.com>
2018-03-31 23:33:00 +0800

    更新代理列表-调试

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-from proxyspider import zdaye, goubanjia, kuaidaili, xici, cn66, ip89cn
-from proxyspider import ip3366, data5u, ip181com, yaoyaodaili, kaixindaili, xdailicn
+from proxyspider import kuaidaili, xici, cn66, ip89cn
+from proxyspider import ip3366, data5u, coderbusy, horocn
 from proxytest import proxytest
 import threading
 import Queue
@@ -18,30 +18,23 @@ opts, args = parser.parse_args()
 
 
 def spidermain():
-    zdayefree = zdaye()
-    zdayefree.zdylist()
-    goubanjiafree = goubanjia()
-    goubanjiafree.proxyre()
-    kdaili = kuaidaili()
-    kdaili.inhaproxyre()
-    xicispider = xici()
-    xicispider.wtproxyre()
-    cn66spider = cn66()
-    cn66spider.proxyre()
-    cn89spider = ip89cn()
-    cn89spider.proxyre()
-    ip36ydl = ip3366()
-    ip36ydl.proxyre()
-    data5uc = data5u()
-    data5uc.proxyre()
-    ip181 = ip181com()
-    ip181.proxyre()
-    yaoyao = yaoyaodaili()
-    yaoyao.proxyre()
-    kxdl = kaixindaili()
-    kxdl.proxyre()
-    xundaili = xdailicn()
-    xundaili.proxyre()
+    #kdaili = kuaidaili()
+    #kdaili.inhaproxyre()
+    #xicispider = xici()
+    #xicispider.wtproxyre()
+    #cn66spider = cn66()
+    #cn66spider.proxyre()
+    #cn89spider = ip89cn()
+    #cn89spider.proxyre()
+    #ip36ydl = ip3366()
+    #ip36ydl.proxyre()
+    #data5uc = data5u()
+    #data5uc.proxyre()
+    #coderbusydaili = coderbusy()
+    #coderbusydaili.proxyre()
+    #horocnproxy = horocn()
+    #horocnproxy.proxyre()
+
     print "spider done"
 
 def proxy_test():
--- proxyspider.py
+++ proxyspider.py
@@ -8,13 +8,8 @@ import os
 import time
 import requests
 import json
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from model import sqlite
+import string
 
 db = sqlite()
 
@@ -28,100 +23,12 @@ class request():
         while fails < 31:
             try:
                 request = requests.get(url, headers=headers, verify=self.verify, timeout=self.timeout)
+                print(request.text)
                 return request.text
             except Exception as e:
                 print e
                 print "error retry"
                 fails += 1
-
-
-# 站大爷
-class zdaye():
-    def __init__(self):
-        self.headers = {
-            "Referer": "http://ip.zdaye.com/dayProxy.html",
-            "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-        }
-        self.url = "http://ip.zdaye.com/dayProxy.html"
-    
-    def zdylist(self):
-        getHtml = request()
-        print 'process zdaye.com'
-        for page in range(1, 3):
-            if page == 1:
-                url = self.url
-            else:
-                url = "http://ip.zdaye.com/dayProxy/" + str(page) + ".html"
-            pagescode = getHtml.r(url, self.headers)
-            soup = BeautifulSoup(pagescode, "html.parser")
-            proxyhtml = soup.select("div.title > a")
-            for href in proxyhtml:
-                zdyurl = "http://ip.zdaye.com" + href.get('href')
-                self.proxyre(zdyurl)
-                time.sleep(2)
-
-    def proxyre(self, url):
-        getHtml = request()
-        pagescode = getHtml.r(url, self.headers)
-        ipre = r"(((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?):\d*)"
-        result = re.findall(ipre, pagescode)
-        for a in result:
-            ipaddr = str(a[0]).split(':')[0]
-            port = str(a[0]).split(':')[1]
-            print ipaddr, port
-            db.insertdb(ipaddr, port)
-
-
-#全网代理IP
-class goubanjia():
-    def __init__(self):
-        self.dcap = DesiredCapabilities.PHANTOMJS.copy()
-        self.dcap['phantomjs.page.customHeaders.Referer'] = 'http://www.goubanjia.com/free/index.shtml'
-        self.dcap["phantomjs.page.settings.userAgent"] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
-        self.driver = webdriver.PhantomJS(
-            executable_path='lib/phantomjs', 
-            desired_capabilities=self.dcap)
-        self.url = "http://www.goubanjia.com/free/gngn/index.shtml"
-    
-    def proxyre(self):
-        print "process guobanjia.com"
-        self.driver.get(self.url)
-        try:
-            WebDriverWait(self.driver, 60).until(
-                EC.presence_of_element_located(By.ID, "wp-pagenavi")
-            )
-        except Exception as e:
-            pass
-        finally:
-            pagesoup = BeautifulSoup(self.driver.page_source, "html.parser")
-            for pagenumber in range(1, 50):
-                url = "http://www.goubanjia.com/free/gngn/index" + str(pagenumber) + ".shtml"
-                print url
-                self.driver.get(url)
-                try:
-                    WebDriverWait(self.driver, 60).until(
-                        EC.presence_of_element_located(By.ID, "wp-pagenavi")
-                        )
-                except Exception as e:
-                    pass
-                finally:
-                    pagescode = self.driver.page_source
-                    soup = BeautifulSoup(pagescode, "html.parser")
-                    dellist = soup.select("div#list > table.table > tbody > tr > td.ip > p")
-                    for delnode in dellist:
-                        delnode.decompose()
-                    iplist = soup.select("div#list > table.table > tbody > tr > td.ip")
-                    for ip in iplist:
-                        ipport = str(ip.get_text()).split(":")
-                        if len(ipport) == 2:
-                            ipaddr = ipport[0]
-                            port = ipport[1]
-                            print ipaddr, port
-                            db.insertdb(ipaddr, port)
-                        else:
-                            pass
-            self.driver.quit()
 
 
 # 快代理
@@ -156,14 +63,15 @@ class xici():
 
     def __init__(self):
         self.headers = {
-            "Referer": "http://www.xicidaili.com",
-            "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+            "Referer": "http://www.xicidaili.com/api",
+            "Host": "www.xicidaili.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/5"
         }
     
     def wtproxyre(self):
         print "process xicidaili"
         getHtml = request()
+        print(getHtml)
         for page in range(1, 50):
             url = "http://www.xicidaili.com/nn/" + str(page)
             pagescode = getHtml.r(url, self.headers)
@@ -184,7 +92,8 @@ class cn66():
         self.headers = {
             "Referer": "http://www.66ip.cn",
             "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            "Host": "www.66ip.cn"
         }
 
     def proxyre(self):
@@ -207,7 +116,8 @@ class ip89cn():
         self.headers = {
             "Referer": "http://www.89ip.cn",
             "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            "Host": "www.89ip.cn"
         }
         self.url = r"http://www.89ip.cn/tiqv.php?sxb=&tqsl=10000&ports=&ktip=&xl=on&submit=%CC%E1++%C8%A1"
     
@@ -226,25 +136,24 @@ class ip89cn():
 class ip3366():
     def __init__(self):
         self.headers = {
-            "Referer": "http://www.ip3366.com/free/",
+            "Referer": "http://www.ip3366.net/apidoc/",
             "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            "Host": "www.ip3366.net"
         }
-        self.url = "http://www.ip3366.net/free/?stype=1&page=1"
 
     def proxyre(self):
         getHtml = request()
-        for pagenumber in range(1, 6):
-            url = "http://www.ip3366.net/free/?stype=1&page=" + str(pagenumber)
-            pagescode = getHtml.r(url, self.headers)
-            soup = BeautifulSoup(pagescode, "html.parser")
-            iplist = soup.select('div#list > table > tbody > tr > td:nth-of-type(1)')
-            portlist = soup.select('div#list > table > tbody > tr > td:nth-of-type(2)')
-            for x in range(0, (len(iplist))):
-                ipaddr = iplist[x].get_text()
-                port = portlist[x].get_text()
-                print ipaddr, port
-                db.insertdb(ipaddr, port)
+        url = "http://www.ip3366.net/free/"
+        pagescode = getHtml.r(url, self.headers)
+        soup = BeautifulSoup(pagescode, "html.parser")
+        iplist = soup.select('div#list > table > tbody > tr > td:nth-of-type(1)')
+        portlist = soup.select('div#list > table > tbody > tr > td:nth-of-type(2)')
+        for x in range(0, (len(iplist))):
+            ipaddr = iplist[x].get_text()
+            port = portlist[x].get_text()
+            print ipaddr, port
+            db.insertdb(ipaddr, port)
 
 #data5u
 class data5u():
@@ -252,7 +161,8 @@ class data5u():
         self.headers = {
             "Referer": "http://www.data5u.com",
             "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            "Host": "www.data5u.com"
         }
         self.url = "http://www.data5u.com/free/gngn/index.shtml"
 
@@ -268,94 +178,49 @@ class data5u():
             print ipaddr, port
             db.insertdb(ipaddr, port)
 
-#ip181
-class ip181com():
+class coderbusy():
     def __init__(self):
         self.headers = {
-            "Referer": "https://www.baidu.com",
+            "Referer": "https://proxy.coderbusy.com/classical/anonymous-type/highanonymous.aspx",
             "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            "Host": "proxy.coderbusy.com"
         }
-        self.url = "http://www.ip181.com/"
+
+
+    def proxyre(self):
+        getHtml = request()
+        for i in range(1, 25):
+            url = "https://proxy.coderbusy.com/classical/country/cn.aspx?page=" + str(i)
+            pagescode = getHtml.r(url, self.headers)
+            soup = BeautifulSoup(pagescode, "html.parser")
+            iplist = soup.select("div.table-responsive > table.table > tbody > tr > td:nth-of-type(1)")
+            portlist = soup.select("div.table-responsive > table.table > tbody > tr > td:nth-of-type(3)")
+            for x in range(0, (len(iplist))):
+                ipaddr = string.strip(iplist[x].get_text())
+                port = string.strip(portlist[x].get_text())
+                print ipaddr, port
+                db.insertdb(ipaddr, port)
+
+class horocn():
+    def __init__(self):
+        self.headers = {
+            "Referer": "https://proxy.horocn.com/free-proxy.html",
+            "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            "Host": "proxy.horocn.com"
+        }
+        self.url = "https://proxy.horocn.com/free-proxy.html?loc_name=%E4%B8%AD%E5%9B%BD"
 
     def proxyre(self):
         getHtml = request()
         pagescode = getHtml.r(self.url, self.headers)
         soup = BeautifulSoup(pagescode, "html.parser")
-        iplist = soup.select("div > table > tbody > tr > td:nth-of-type(1)")
-        portlist = soup.select("div > table > tbody > tr > td:nth-of-type(2)")
-        for x in range(0, len(iplist)):
-            ipaddr = iplist[x].get_text()
-            port = portlist[x].get_text()
+        iplist = soup.select("div.bs-docs-section > table.table > tbody > tr > th:nth-of-type(1)")
+        portlist = soup.select("div.bs-docs-section > table.table > tbody > tr > th:nth-of-type(2)")
+        for x in range(0, (len(iplist))):
+            ipaddr = string.strip(iplist[x].get_text())
+            port = string.strip(portlist[x].get_text())
             print ipaddr, port
             db.insertdb(ipaddr, port)
 
-#nttpsdaili
-class yaoyaodaili():
-    def __init__(self):
-        self.headers = {
-            "Referer": "http://www.httpsdaili.com",
-            "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-        }
-        self.url = "http://www.httpsdaili.com"
-
-    def proxyre(self):
-        getHtml = request()
-        for pagenumber in range(1, 6):
-            url = "http://www.httpsdaili.com/free.asp?page=" + str(pagenumber)
-            pagescode = getHtml.r(url, self.headers)
-            soup = BeautifulSoup(pagescode, "html.parser")
-            iplist = soup.select("div#list > table > tbody > tr > td:nth-of-type(1)")
-            portlist = soup.select("div#list > table > tbody > tr > td:nth-of-type(2)")
-            for x in range(0, len(iplist)):
-                ipaddr = iplist[x].get_text()
-                port = portlist[x].get_text()
-                print ipaddr, port
-                db.insertdb(ipaddr, port)
-
-
-#kxdaili.com
-class kaixindaili():
-    def __init__(self):
-        self.headers = {
-            "Referer": "http://www.kxdaili.com/dailiip/1/1.html#ip",
-            "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-        }
-        self.url = "http://www.kxdaili.com/dailiip/1/1.html#ip"
-
-    def proxyre(self):
-        getHtml = request()
-        for pagenumber in range(1, 10):
-            url = "http://www.kxdaili.com/dailiip/1/" + str(pagenumber) + ".html#ip"
-            pagescode = getHtml.r(url, self.headers)
-            soup = BeautifulSoup(pagescode, "html.parser")
-            iplist = soup.select("div > table > tbody > tr > td:nth-of-type(1)")
-            portlist = soup.select("div > table > tbody > tr > td:nth-of-type(2)")
-            for x in range(0, len(iplist)):
-                ipaddr = iplist[x].get_text()
-                port = portlist[x].get_text()
-                print ipaddr, port
-                db.insertdb(ipaddr, port)
-
-#xdaili.cn
-class xdailicn():
-    def __init__(self):
-        self.headers = {
-            "Referer": "http://www.xdaili.cn/freeproxy",
-            "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-        }
-        self.url = "http://www.xdaili.cn/ipagent//freeip/getFreeIps?page=1&rows=10"
-
-    def proxyre(self):
-        getHtml = request()
-        pagescode = getHtml.r(self.url, headers=self.headers)
-        data = json.loads(pagescode)
-        rows = data["RESULT"]["rows"]
-        for x in rows:
-            ipaddr = x["ip"]
-            port = x["port"]
-            print ipaddr, port
-            db.insertdb(ipaddr, port)
